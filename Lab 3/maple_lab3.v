7d6147bb6e58d8089bf6beada1541fe9c030f958
devanchanda <devanchanda2026@u.northwestern.edu>
2025-11-10 21:04:47 -0600

    Final Push

--- Lab 3/maple_lab3.v
+++ Lab 3/maple_lab3.v
@@ -14,9 +14,9 @@
 `define OPCODE_IMM      7'b0010011
 // B-Type
 `define OPCODE_BRANCH   7'b1100011
-// I-Type Loads
+// I-Type loads
 `define OPCODE_LOAD     7'b0000011
-// S-Type Store
+// S-Type store
 `define OPCODE_STORE    7'b0100011 
 // U-Type lui
 `define OPCODE_LUI      7'b0110111
@@ -37,7 +37,7 @@
 `define FUNC_OR         3'b110
 `define FUNC_AND        3'b111
 
-// funct3 Branch
+// funct3 branch
 `define FUNC_BEQ        3'b000
 `define FUNC_BNE        3'b001
 `define FUNC_BLT        3'b100
@@ -45,7 +45,7 @@
 `define FUNC_BLTU       3'b110
 `define FUNC_BGEU       3'b111
 
-// funct3 Load/Store
+// funct3 load/store
 `define FUNC_LB_SB      3'b000
 `define FUNC_LH_SH      3'b001
 `define FUNC_LW_SW      3'b010
@@ -67,7 +67,7 @@
 `define FUNC_REM        3'b110
 `define FUNC_REMU       3'b111
 
-// Memory Sizes
+// memory sizes
 `define SIZE_BYTE  2'b00
 `define SIZE_HWORD 2'b01
 `define SIZE_WORD  2'b10
@@ -81,28 +81,28 @@ module SingleCycleCPU(halt, clk, rst);
    output halt;
    input clk, rst;
 
-   // Instruction Fetch (IF)
-   // Holding the current PC, Fetching the instruction word from the instruction
-   // memory, and Computing PC + 4 for the next sequential instruction
+   // IF
+   // holds the current PC, fetches instruction from im
+   // computes PC + 4 for the next sequential instruction
 
-   // PC holds fetch address; InstWord is fetch instruction
+   // PC holds fetch address, InstWord is fetch instruction
    wire [`WORD_WIDTH-1:0] PC, InstWord;
 
-   // Fall-through address used by PC mux and jal/jalr writeback
+   // fall through address used by PC mux and jal/jalr writeback
    wire [`WORD_WIDTH-1:0] PC_Plus_4 = PC + 32'd4;
 
 
-   // Memory (MEM) on the data side
+   // MEM on the data side
    wire [`WORD_WIDTH-1:0] DataAddr, StoreData, DataWord;
    wire [1:0]  MemSize;
    wire        MemWrEn;
 
-   // ID/EX?WB: Register File
+   // ID/EX/WB register file
    wire [4:0]  Rsrc1, Rsrc2, Rdst;
    wire [`WORD_WIDTH-1:0] Rdata1, Rdata2, RWrdata;
    wire        RWrEn;
 
-   // Instruction Decode (ID)
+   // ID
    wire [6:0]  opcode;
    wire [6:0]  funct7;
    wire [2:0]  funct3;
@@ -111,11 +111,11 @@ module SingleCycleCPU(halt, clk, rst);
    assign Rdst = InstWord[11:7]; 
    assign Rsrc1 = InstWord[19:15]; 
    assign Rsrc2 = InstWord[24:20];
-   assign funct3 = InstWord[14:12];  // R-Type, I-Type, S-Type
-   assign funct7 = InstWord[31:25];  // R-Type
+   assign funct3 = InstWord[14:12];
+   assign funct7 = InstWord[31:25];
 
 
-   // Immediate Generators per type
+   // immediate generators per type
    wire [`WORD_WIDTH-1:0] immI = {{20{InstWord[31]}}, InstWord[31:20]};
    wire [`WORD_WIDTH-1:0] immS = {{20{InstWord[31]}}, InstWord[31:25], InstWord[11:7]};
    // b/j immediates are scrambled and implicityly << by 1 (LSB = 0)
@@ -123,7 +123,7 @@ module SingleCycleCPU(halt, clk, rst);
    wire [`WORD_WIDTH-1:0] immU = {InstWord[31:12], 12'b0};
    wire [`WORD_WIDTH-1:0] immJ = {{11{InstWord[31]}}, InstWord[31], InstWord[19:12], InstWord[20], InstWord[30:21], 1'b0};
 
-   // Instruction Classification (control decode)
+   // instruction classification
    wire is_rtype = (opcode == `OPCODE_COMPUTE);
    wire is_itype = (opcode == `OPCODE_IMM);
    wire is_load = (opcode == `OPCODE_LOAD);
@@ -186,7 +186,7 @@ module SingleCycleCPU(halt, clk, rst);
    wire is_rem = is_rtype & (funct7 == `M_FUNC7) & (funct3 == `FUNC_REM);
    wire is_remu = is_rtype & (funct7 == `M_FUNC7) & (funct3 == `FUNC_REMU);
 
-   // ID/EX: ALU operand selection
+   // ID/EX ALU operand selection
    // opA = rs1 (from RF); opB = rs2 or imm
    // for stores, addr = rs1 + immS
    // for loads/I-ops, = rs1 + immI
@@ -206,7 +206,7 @@ module SingleCycleCPU(halt, clk, rst);
                               is_rtype ? funct7 : 7'b0000000;
    
 
-   // Execution Unit (EX) - ALU/shift/M
+   // EX - ALU/shift/M
    // Produces ALU result used for arithmetic/logic, effective addr for load + stores,
    // and branch target adders use PC + immB
 
@@ -220,7 +220,7 @@ module SingleCycleCPU(halt, clk, rst);
       .auxFunc(ALU_auxFunc)
    );
 
-   // Data Memory Access (MEM)
+   // MEM data memory access
    // DataAddr = effective address from ALU
    // StoreData = rs2 (value to write on stores)
    // MemSize selects byte/half/word at memory
@@ -232,7 +232,7 @@ module SingleCycleCPU(halt, clk, rst);
                     (is_lh | is_lhu | is_sh) ? `SIZE_HWORD :
                                                `SIZE_WORD;
 
-   // MEM --> WB (Load Extender)
+   // MEM --> WB (load extender)
    // Extract bytes based on address alignment
    wire [7:0]  byte0 = DataWord[7:0];
    wire [7:0]  byte1 = DataWord[15:8];
@@ -241,7 +241,7 @@ module SingleCycleCPU(halt, clk, rst);
    wire [15:0] hword0 = DataWord[15:0];
    wire [15:0] hword1 = DataWord[31:16];
 
-   // Select correct byte based on address alignment
+   // selects correct byte based on address alignment
    wire [7:0]  loaded_byte =  (DataAddr[1:0]==2'b00) ? byte0 :
                               (DataAddr[1:0]==2'b01) ? byte1 :
                               (DataAddr[1:0]==2'b10) ? byte2 : byte3;
@@ -257,7 +257,7 @@ module SingleCycleCPU(halt, clk, rst);
                                                         DataWord; // lw
 
    
-   // Writeback mux to rd (WB)
+   // writeback mux to rd (WB)
    wire [`WORD_WIDTH-1:0] auipc_result = PC + immU;
    wire [`WORD_WIDTH-1:0] WB_data = is_any_load ? LoadData : ALUResult;
 
@@ -268,7 +268,7 @@ module SingleCycleCPU(halt, clk, rst);
    wire rd_is_x0 = (Rdst == 5'd0);
 
 
-   // IF/ID/EX: Branch Compare
+   // IF/ID/EX branch compare
    wire signed_lt = ($signed(Rdata1) < $signed(Rdata2));
    wire unsigned_lt = (Rdata1 < Rdata2);
 
@@ -286,7 +286,7 @@ module SingleCycleCPU(halt, clk, rst);
                               PC_Plus_4;
    
 
-   // Misalignment + Illegal Detection
+   // misalignment + illegal detection
    wire fetch_misalign = |PC[1:0];
    wire hword_misalign = (is_lh | is_lhu | is_sh) & DataAddr[0];
    wire word_misalign = (is_lw | is_sw) & |DataAddr[1:0];
@@ -312,7 +312,7 @@ module SingleCycleCPU(halt, clk, rst);
    assign RWrEn = RWrEn_pre   & ~halt & ~rd_is_x0;
    assign RWrdata = RWrdata_pre;
 
-   // Instruction Memory (IF) - System State
+   // IF system state
    Mem   MEM(.InstAddr(PC), .InstOut(InstWord), 
             .DataAddr(DataAddr), .DataSize(MemSize), .DataIn(StoreData), .DataOut(DataWord), .WE(MemWrEn), .CLK(clk));
 
@@ -334,14 +334,14 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
    input [2:0] 	 func;
    input [6:0] 	 auxFunc;
 
-   // Shifts
+   // shifts
    wire [4:0] shamt = opB[4:0];
 
-   // Signed/Unsigned
+   // signed/unsigned
    wire signed [31:0] opA_s = opA;
    wire signed [31:0] opB_s = opB;
 
-   // Base ALU ops
+   // base ALU ops
    wire [31:0] add = opA + opB;
    wire [31:0] sub = opA - opB;
    wire [31:0] _and = opA & opB;
@@ -353,7 +353,7 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
    wire [31:0] slt = (opA_s < opB_s) ? 32'd1 : 32'd0;
    wire [31:0] sltu = (opA < opB  ) ? 32'd1 : 32'd0;
 
-   // Multiplication
+   // multiplication
    // signed * signed
    wire [63:0] mul_ss = $signed(opA) * $signed(opB);
    // unsigned * unsigned
@@ -361,7 +361,7 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
    // signed * unsigned
    wire [63:0] mul_su = $signed(opA) * $unsigned(opB);
 
-   // Division
+   // division
    wire div_by_zero  = (opB == 32'b0);
    wire div_overflow = (opA == 32'h8000_0000) && (opB == 32'hFFFF_FFFF);
 
@@ -380,7 +380,7 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
                         (opA % opB);
 
 
-   // Result Selection
+   // result selection
    assign out =
       (auxFunc==7'b0000000 && func==3'b000) ? add :
       (auxFunc==7'b0000000 && func==3'b001) ? sll :
